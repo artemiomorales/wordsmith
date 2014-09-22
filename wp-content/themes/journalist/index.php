081bb50668024669888ecf65d7cb11ebeb00f2c8
ricardomorales <ric.morales22@gmail.com>
2014-09-21 22:37:54 -0400

    Altered template files to query projects and entries separately.

--- wp-content/themes/journalist/index.php
+++ wp-content/themes/journalist/index.php
@@ -1,41 +1,80 @@
 <?php get_header(); ?>
+<?php $query1 = new WP_Query( 'category_name=entries' ); ?>
+<div id="content-prototypes" class="content group">
+	<?php if ($query1->have_posts()) : while ($query1->have_posts()) : $query1->the_post(); ?>
 
-<div id="content" class="group">
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+	<!-- <h2 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h2> -->
+	<h2 id="post-<?php the_ID(); ?>"><?php the_title(); ?></h2>
+	<!-- <p class="comments"><a href="<?php comments_link(); ?>"><?php comments_number('without comments','with one comment','with % comments'); ?></a></p> -->
 
-<h2 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-<p class="comments"><a href="<?php comments_link(); ?>"><?php comments_number('without comments','with one comment','with % comments'); ?></a></p>
+	<div class="main">
+		<?php the_content('Read the rest of this entry &raquo;'); ?>
+	</div>
 
-<div class="main">
-	<?php the_content('Read the rest of this entry &raquo;'); ?>
+	<div class="meta group">
+	<div class="signature">
+	    <p>Written by <?php the_author() ?> <span class="edit"><?php edit_post_link('Edit'); ?></span></p>
+	    <p><?php the_time('F jS, Y'); ?> <?php _e("at"); ?> <?php the_time('g:i a'); ?></p>
+	</div>	
+	<div class="tags">
+	    <p>Posted in <?php the_category(',') ?></p>
+	    <?php if ( the_tags('<p>Tagged with ', ', ', '</p>') ) ?>
+	</div>
+	</div>
+
+	<?php if ( comments_open() ) comments_template(); ?>
+
+	<?php endwhile; else: ?>
+	<div class="warning">
+		<p>Sorry, but you are looking for something that isn't here.</p>
+	</div>
+	<?php endif; ?>
+
+	<div class="navigation group">
+		<div class="alignleft"><?php next_posts_link('&laquo; Older Entries') ?></div>
+		<div class="alignright"><?php previous_posts_link('Newer Entries &raquo;') ?></div>
+	</div>
+
 </div>
 
-<div class="meta group">
-<div class="signature">
-    <p>Written by <?php the_author() ?> <span class="edit"><?php edit_post_link('Edit'); ?></span></p>
-    <p><?php the_time('F jS, Y'); ?> <?php _e("at"); ?> <?php the_time('g:i a'); ?></p>
-</div>	
-<div class="tags">
-    <p>Posted in <?php the_category(',') ?></p>
-    <?php if ( the_tags('<p>Tagged with ', ', ', '</p>') ) ?>
-</div>
-</div>
+<?php wp_reset_postdata(); ?>
+<?php $query2 = new WP_Query( 'category_name=projects' ); ?>
 
-<?php if ( comments_open() ) comments_template(); ?>
+<div id="content-entries" class="content group">
+	<?php if ($query2->have_posts()) : while ($query2->have_posts()) : $query2->the_post(); ?>
 
-<?php endwhile; else: ?>
-<div class="warning">
-	<p>Sorry, but you are looking for something that isn't here.</p>
-</div>
-<?php endif; ?>
+	<!-- <h2 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h2> -->
+	<h2 id="post-<?php the_ID(); ?>"><?php the_title(); ?></h2>
+	<!-- <p class="comments"><a href="<?php comments_link(); ?>"><?php comments_number('without comments','with one comment','with % comments'); ?></a></p> -->
 
-<div class="navigation group">
-	<div class="alignleft"><?php next_posts_link('&laquo; Older Entries') ?></div>
-	<div class="alignright"><?php previous_posts_link('Newer Entries &raquo;') ?></div>
-</div>
+	<div class="main">
+		<?php the_content('Read the rest of this entry &raquo;'); ?>
+	</div>
+
+	<div class="meta group">
+	<div class="signature">
+	    <p>Written by <?php the_author() ?> <span class="edit"><?php edit_post_link('Edit'); ?></span></p>
+	    <p><?php the_time('F jS, Y'); ?> <?php _e("at"); ?> <?php the_time('g:i a'); ?></p>
+	</div>	
+	<div class="tags">
+	    <p>Posted in <?php the_category(',') ?></p>
+	    <?php if ( the_tags('<p>Tagged with ', ', ', '</p>') ) ?>
+	</div>
+	</div>
+
+	<?php if ( comments_open() ) comments_template(); ?>
+
+	<?php endwhile; else: ?>
+	<div class="warning">
+		<p>Sorry, but you are looking for something that isn't here.</p>
+	</div>
+	<?php endif; ?>
+
+	<div class="navigation group">
+		<div class="alignleft"><?php next_posts_link('&laquo; Older Entries') ?></div>
+		<div class="alignright"><?php previous_posts_link('Newer Entries &raquo;') ?></div>
+	</div>
 
 </div> 
-
-<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
